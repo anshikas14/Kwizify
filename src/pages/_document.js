fcfc9ecd5a48a2b3665ece892e815634ae948542
Anshika <124166933+anshikas14@users.noreply.github.com>
2025-05-22 21:03:17 +0530

    improved the social cto⚙️

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,7 @@ export default function Document () {
 				<link rel="icon" href="/favicon.svg" />
 
 				{/* <!-- Meta --> */}
-				<meta name="author" content="Cosmo" />
+				<meta name="author" content="Anshika" />
 				<meta name="theme-color" content="#3b82f6" />
 				<meta name="description" content="Kwizify is a quiz/trivia game with questions made by IA. You can select different game modes and topics, you can also use different wildcards to help you answer the questions." />
 				<meta name="robots" content="index, nofollow" />
@@ -17,16 +17,10 @@ export default function Document () {
 				<meta property="og:title" content="Kwizify" />
 				<meta property="og:type" content="website" />
 				<meta property="og:image" content="https://repository-images.githubusercontent.com/593410779/0555ac47-4bd6-49a9-8869-5b1ec84a58f6" />
-				<meta property="og:url" content="https://kwizify.vercel.app" />
+				<meta property="og:url" content="https://https://kwizify-five.vercel.app/.app" />
 				<meta property="og:description" content="Kwizify is a quiz/trivia game with questions made by IA. You can select different game modes and topics, you can also use different wildcards to help you answer the questions." />
 
-				{/* <!-- Twitter --> */}
-				<meta name="twitter:card" content="summary_large_image" />
-				<meta name="twitter:site" content="@Cosmoart" />
-				<meta name="twitter:creator" content="@Cosmoart" />
-				<meta name="twitter:title" content="Kwizify" />
-				<meta name="twitter:description" content="Kwizify is a quiz/trivia game with questions made by IA. You can select different game modes and topics, you can also use different wildcards to help you answer the questions." />
-				<meta name="twitter:image" content="https://repository-images.githubusercontent.com/593410779/0555ac47-4bd6-49a9-8869-5b1ec84a58f6" />
+				
 			</Head>
 			<body>
 				<Main />
